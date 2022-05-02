f7a2ef85c5f4d7eb195b8381756f5d0d6ba11502
SeanCh7 <62511636+SeanCh7@users.noreply.github.com>
2022-05-02 13:58:44 -0500

    Update tft.ino

--- testboard/RIODuino/tft.ino
+++ testboard/RIODuino/tft.ino
@@ -1,12 +1,18 @@
 void setup() {
     Wire.begin(4);
     Wire.onReceive(receiveEvent);
+    Serial.begin(9600)
 }
 
 void loop() {
 
 }
 
-void receiveEvent() {
-    
+void receiveEvent(int howMany) {
+     while (Wire.available()) {
+          char n = (char)Wire.read()
+          if (((int)n) > ((int)(' '))) {
+              Serial.println(n)
+              }
+     }
 }
